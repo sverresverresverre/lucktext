26da61fbfdaa8e92105e632bf7a28f49f5542486
Sverre Källberg <sverre.kallberg@elev.ga.ntig.se>
2023-09-05 10:52:27 +0200

    Lucktext

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("Skriv in ditt namn:");
+string name = Console.ReadLine().ToLower();
+
+
+if (name == "sverre")
+{
+   Console.WriteLine("Välkommen!"); 
+}
+else if (name == "micke")
+{
+    Console.WriteLine("Tja");
+}
+else
+{
+    Console.WriteLine("Jävla jävla din jävla du är en jävla");
+}
+
+Console.WriteLine("Skriv vem som är bäst på att chilla");
+
+string person = Console.ReadLine().ToLower();
+
+
+if (person == "pelikanen")
+{
+    Console.WriteLine("Men ja, helt rätt, du kan så bra");
+}
+
+else
+{
+    Console.WriteLine("Näää du tror men du vet inget!");
+}
+
+Console.WriteLine("Sista frågan, vad är din djupaste hemlighet?");
+
+Console.ReadLine();
+
+Console.WriteLine("Tack för att du delar med dig");
+
+
+
+
+
+Console.ReadLine();
